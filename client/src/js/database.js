c751f3e3d30a1c0b341e8adea366bc2bde642a40
Mitra USYD <mitraahmadi129@gmail.com>
2024-08-05 15:30:52 +1000

    fixing up mistakes made in the crud functions

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -20,7 +20,7 @@ export const putDb = async (content) => {
     const store = tx.objectStore("jate");
     const request = store.put({ id: 1, content });
     const result = await request;
-    console.log(`result.value` result);
+    console.log(`result.value`, result);
     return result;
   } catch (err) {
     console.error("putDb not implemented");
